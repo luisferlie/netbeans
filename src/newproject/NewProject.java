c4552cb430ab20dc62211cb839782b60338694a0
luisferlie <luisferlie@gmail.com>
2021-02-17 17:54:25 +0100

    r commit

---
+++ src/newproject/NewProject.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newproject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Luis
+ */
+public class NewProject extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
