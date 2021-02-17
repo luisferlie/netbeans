f3d745ffa7a326af50d4afb5bf481f2631597662
luisferlie <luisferlie@gmail.com>
2021-02-17 18:20:29 +0100

    3commit

--- src/newproject/NewProject.java
+++ src/newproject/NewProject.java
@@ -14,7 +14,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class NewProject extends Application {
-//blabla
+//blablabla
 
     @Override
     public void start(Stage primaryStage) {
